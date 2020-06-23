1a22748e9a7f9dd306f921a15f97e9be4f1f1b3c
Quentin <quentin.muzel@etu.univ-lyon1.fr>
2020-06-23 21:51:15 +0200

    add 2 etape, modif css

---
+++ Etape_1/generateur.js
@@ -0,0 +1,21 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+function generateur(){
+  const debutT1 = ["J'ai un rêve à accomplir,"  , "Je n'abandonnerai jamais,"  ,"N'abandonne pas,"  ,"Je ne vais pas m'enfuir en courant,"  ,"Un rêve irréalisable?"  ,"Un jour nous prendrons la mer "  ,"Continue ta route,"  ," Un jour, sans aucun doute,"  ,"Je les exterminerai tous,"  ,"Il ne faut pas me sous-estimer,"];
+  const  milieuT1 = ["si je meurs en essayant,"  , "aussi longtemps que je continuerai à respirer dans ce monde,"  ,"le début est toujours le moment le plus difficile,"  ,"je ne reviens jamais sur mes mots,"  ,"c'est ça l'aventure,"  ,"et nous vivrons notre vie comme nous le voulons,"  ,"observe le monde de tes propres yeux et alors,"  ,"des amis qui te protégerons apparaîtront,"  ,"un à un jusqu'au dernier,"  ,"je vais tuer ce titan"];
+  const finT1 = ["au moins j'aurai essayé !"  , "aussi longtemps que je tiendrai debout !"  ,"alors faisons de notre mieux jusqu'à la toute fin !"  ,"c'est mon nindô, ma devise de ninja ! "  ,"si on laisse passer cette occasion on le regrettera tout le restant de notre vie."
+    ,"plus libre que quiconque !"  ,"peut-être qu'à la fin de ton voyage tu parviendras à une conclusion différente de la nôtre."  ,"personne est né dans ce monde pour être tout seul !"  ,"de mes propres mains..."  ,"et vous sauver tous les trois"];
+
+  document.getElementById("citationZone").innerHTML = "" ;
+  document.getElementById("titre1").style.display = "block";
+  document.getElementById("citationZone").innerHTML += debutT1[getRandomInt(debutT1.length)] +" "+ milieuT1[getRandomInt(milieuT1.length)] +" "+ finT1[getRandomInt(finT1.length)]+"<br>" ;
+
+}
+
+
+
+document.getElementById("btn1").addEventListener('click',generateur,false);
+
